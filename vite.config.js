9b577c8a3ae84e764da6bc95e41ac25f2a5bccc4
Paul Brown <browningtons@gmail.com>
2026-01-06 22:52:36 -0800

    update name of repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/youth_sports_app/",
+  base: "/youth-sports-app/",
 })
